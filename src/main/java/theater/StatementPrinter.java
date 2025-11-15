48c500ba1b33daf162f9d6ee05e13d205b45ff14
Nesreyli <rajicicmarko1@gmail.com>
2025-11-15 15:44:26 -0500

    Task 2.2

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -23,7 +23,7 @@ public class StatementPrinter {
      */
     public String statement() {
         int totalAmount = 0;
-        int volumeCredits = 0;
+        int rslt = 0;
         final StringBuilder result =
                 new StringBuilder("Statement for " + invoice.getCustomer() + System.lineSeparator());
 
@@ -31,12 +31,7 @@ public class StatementPrinter {
 
         for (Performance perform : invoice.getPerformances()) {
 
-            // add volume credits
-            volumeCredits += Math.max(perform.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
-            // add extra credit for every five comedy attendees
-            if ("comedy".equals(getPlay(perform).getType())) {
-                volumeCredits += perform.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
-            }
+            rslt = getVolumeCredits(perform, rslt);
 
             // print line for this order
             result.append(String.format(
@@ -48,36 +43,47 @@ public class StatementPrinter {
         }
         result.append(String.format("Amount owed is %s%n",
                 frmt.format(totalAmount / Constants.PERCENT_FACTOR)));
-        result.append(String.format("You earned %s credits%n", volumeCredits));
+        result.append(String.format("You earned %s credits%n", rslt));
         return result.toString();
+    }
+
+    private int getVolumeCredits(Performance perform, int volumeCredits) {
+        // add volume credits
+        int result = 0;
+        result += Math.max(perform.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
+        // add extra credit for every five comedy attendees
+        if ("comedy".equals(getPlay(perform).getType())) {
+            result += perform.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
+        }
+        return result + volumeCredits;
     }
 
     private Play getPlay(Performance perform) {
         return plays.get(perform.getPlayID());
     }
 
-    private int getAmount(Performance perform) {
+    private int getAmount(Performance performance) {
         int thisAmount = 0;
 
-        switch (getPlay(perform).getType()) {
+        switch (getPlay(performance).getType()) {
             case "tragedy":
                 thisAmount = Constants.TRAGEDY_BASE_AMOUNT;
-                if (perform.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
+                if (performance.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
                     thisAmount += Constants.TRAGEDY_OVER_BASE_CAPACITY_PER_PERSON
-                            * (perform.getAudience() - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
+                            * (performance.getAudience() - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
                 }
                 break;
             case "comedy":
                 thisAmount = Constants.COMEDY_BASE_AMOUNT;
-                if (perform.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
+                if (performance.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
                     thisAmount += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT
                             + (Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON
-                            * (perform.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD));
+                            * (performance.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD));
                 }
-                thisAmount += Constants.COMEDY_AMOUNT_PER_AUDIENCE * perform.getAudience();
+                thisAmount += Constants.COMEDY_AMOUNT_PER_AUDIENCE * performance.getAudience();
                 break;
             default:
-                throw new RuntimeException(String.format("unknown type: %s", getPlay(perform).getType()));
+                throw new RuntimeException(String.format("unknown type: %s", getPlay(performance).getType()));
         }
         return thisAmount;
     }
